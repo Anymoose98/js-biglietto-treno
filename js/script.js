6a43a8b72cc20ef4be78c738e52614940794b999
Anymoose98 <daniel.digiacomo98@gmail.com>
2023-11-20 18:41:47 +0100

    Esercizio finito

--- js/script.js
+++ js/script.js
@@ -2,19 +2,26 @@ let km = prompt("Quanti km vuoi percorrere?")
 let eta = prompt("Quanti anni hai?")
 
 prezzo_km = (km * 0.21)
-console.log(km)
+console.log(prezzo_km)
 
 if(eta < 18){
     let costo = (prezzo_km - prezzo_km * 0.2)
+    costo = costo.toFixed(2);
     console.log(costo)
+    document.getElementById("costo").innerHTML = document.getElementById("costo").innerHTML + costo
 }
 
-else if(eta > 65){
+else if(eta >= 65){
     let costo = (prezzo_km - prezzo_km * 0.4)
+    costo = costo.toFixed(2);
     console.log(costo)
+    document.getElementById("costo").innerHTML = document.getElementById("costo").innerHTML + costo
+    
 }
 
 else{
     let costo = prezzo_km
+    costo = costo.toFixed(2);
     console.log(costo)
+    document.getElementById("costo").innerHTML = document.getElementById("costo").innerHTML + costo
 }
